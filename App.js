6be2a1f4135703865a9de1f6f021f044b3675f4a
Gustavo Porpino <gporpino@gmail.com>
2019-05-24 04:42:39 -0300

    configured custom fonts

--- App.js
+++ App.js
@@ -1,6 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Routes from './src/routes';
+import { AppLoading, Asset, Font } from 'expo';
 
-const App = () => <Routes />;
+// function cacheImages(images) {
+//   return images.map((image) => {
+//     if (typeof image === 'string') {
+//       return Image.prefetch(image);
+//     }
+//     return Asset.fromModule(image).downloadAsync();
+//   });
+// }
 
-export default App;
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
+export default class App extends Component {
+  state = {
+    isReady: false
+  };
+
+  loadAssetsAsync = async () => {
+    console.log('start');
+    await Font.loadAsync({
+      'sf-pro-display-regular': require('./assets/fonts/sf-pro-display-regular.otf'),
+      'sf-pro-display-bold': require('./assets/fonts/sf-pro-display-bold.otf'),
+      'sf-pro-display-semibold': require('./assets/fonts/sf-pro-display-semibold.otf')
+    });
+
+    this.setState({ fontLoaded: true });
+  };
+
+  finishLoading = () => {
+    console.log('ends');
+    this.setState({ isReady: true });
+  };
+
+  render() {
+    console.log('render');
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this.loadAssetsAsync}
+          onFinish={this.finishLoading}
+        />
+      );
+    } else {
+      console.log('routes');
+      return <Routes />;
+    }
+  }
+}
