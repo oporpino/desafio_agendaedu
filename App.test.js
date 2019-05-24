3ba8673ee35b4737dd5158d6e2091218969b85df
Gustavo Porpino <gporpino@gmail.com>
2019-05-23 23:22:58 -0300

    setup enzyme

--- App.test.js
+++ App.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 import App from './App';
 
 describe('<App />', () => {
   it('has only 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = shallow(<App />);
     expect(tree.children.length).toBe(1);
     expect(tree).toMatchSnapshot();
   });
